b20020b93aef7cdcef72dc4293a5e8d386706142
Lacko12345 <114608511+Lacko12345@users.noreply.github.com>
2022-09-28 19:28:15 +0200

    Create hu.js

---
+++ widget/lang/hu.js
@@ -0,0 +1,20 @@
+export default {
+  powered_by: 'A megjegyzéseket a Cusdis működteti',
+  post_comment: 'Megjegyzés',
+  loading: 'Betöltés',
+
+  // comment
+  email: 'Email (Nem kötelező)',
+  nickname: 'Név',
+  reply_placeholder: 'Válasz...',
+  reply_btn: 'Válasz',
+  sending: 'küldés...',
+
+  // reply
+  mod_badge: 'Moderator',
+  content_is_required: 'A tartalom kötelező',
+  nickname_is_required: 'A Név kötelező',
+
+  comment_has_been_sent:
+    'Megjegyzését elküldtük. Kérjük, várja meg a jóváhagyást.',
+}
